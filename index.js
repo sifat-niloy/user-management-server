bf29c2bd2f435f0fb2017f69832544a43ea6e478
SifatNiloy <sifatniloy18@gmail.com>
2023-09-25 11:22:54 +0600

    new user added in ui from backend

--- index.js
+++ index.js
@@ -1,67 +1,45 @@
 const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
+const cors = require('cors')
+const app = express();
+const port = process.env.PORT || 5000 ;
 
 
 // using middleware
+
+app.use(cors())
 app.use(express.json())
+
 
 const users = [
   {
+    id:1,
     name: "John Smith",
     age: 28,
     email: "john.smith@example.com",
     phone: "+1 (123) 456-7890"
   },
   {
+    id:2,
     name: "Alice Johnson",
     age: 24,
     email: "alice.johnson@example.com",
     phone: "+1 (987) 654-3210"
   },
   {
+    id:3,
     name: "Michael Davis",
     age: 32,
     email: "michael.davis@example.com",
     phone: "+1 (555) 123-4567"
   },
   {
+    id:4,
     name: "Emily Brown",
     age: 29,
     email: "emily.brown@example.com",
     phone: "+1 (777) 888-9999"
   },
-  {
-    name: "Daniel Wilson",
-    age: 35,
-    email: "daniel.wilson@example.com",
-    phone: "+1 (111) 222-3333"
-  },
-  {
-    name: "Sophia Lee",
-    age: 27,
-    email: "sophia.lee@example.com",
-    phone: "+1 (555) 555-5555"
-  },
-  {
-    name: "William Taylor",
-    age: 30,
-    email: "william.taylor@example.com",
-    phone: "+1 (999) 999-9999"
-  },
-  {
-    name: "Olivia Anderson",
-    age: 26,
-    email: "olivia.anderson@example.com",
-    phone: "+1 (888) 888-8888"
-  },
-  {
-    name: "James Miller",
-    age: 31,
-    email: "james.miller@example.com",
-    phone: "+1 (444) 333-2222"
-  },
-  {
+  { id:5,
     name: "Ava Martinez",
     age: 33,
     email: "ava.martinez@example.com",
@@ -70,13 +48,20 @@ const users = [
 ];
 
 
-
 app.get('/', (req, res) => {
   res.send('User management system ')
 })
 
 app.get('/users', (req, res)=>{
     res.send(users)                                                 
+})
+app.post('/users', (req, res)=>{
+  console.log('post api hitting')
+  console.log(req.body)
+  const newUser= req.body
+  newUser.id = users.length + 1;
+  users.push(newUser)
+  res.send(newUser)
 })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
